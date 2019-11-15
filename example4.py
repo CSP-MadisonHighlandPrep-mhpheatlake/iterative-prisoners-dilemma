4bc72236a9fc15fec1c54def6fae47512b21d9e6
NGreub2795 <57366315+NGreub2795@users.noreply.github.com>
2019-11-15 09:48:57 -0700

    Modified Algorithms

--- example4.py
+++ example4.py
@@ -46,4 +46,4 @@ def move(my_history, their_history, my_score, their_score):
         if my_history[-1]=='c' and their_history[-1]=='b':
             return 'b' # Betray if they were severely punished last time
         else:
-            return 'c' # Otherwise collude.
+            return 'b' # Otherwise collude.
