0a731ee8c71ecaf74856c4e386a314a6e050afd2
NotNoithan <57498229+NotNoithan@users.noreply.github.com>
2019-11-20 10:26:13 -0700

    Increased efficiency

--- example6.py
+++ example6.py
@@ -7,27 +7,35 @@
 ####
 
 team_name = 'E6'
-strategy_name = 'Collude until betrayed'
+strategy_name = 'Kill everyone'
 strategy_description = '''\
 Collude first round. Collude, unless betrayed; then always betray.'''
     
 def move(my_history, their_history, my_score, their_score):
     if len(my_history)==0:
         return 'c'
-    elif len(my_history)<5 & len(my_history)>0:
-        if my_history[-1]=='c' and their_history[-1]=='b':
+    elif len(my_history) == 1:
+        if their_history[-1]=='b':
             return 'b'
         else:
             return 'c'
-            
-    last6rounds = their_history[-6:]
+    elif len(my_history)<5 & len(my_history)>0:
+        if my_history[-1]=='c' and their_history[-1]=='b':
+            return 'b'
+        elif my_history[-1]=='b' and their_history[-1]=='b':
+            return 'b'
+        else:
+            return 'c'
+    elif len(my_history) > 5:
+        last6rounds = str(their_history[-6:])
     
-    n = 0
-    for c in last6rounds:
-        n=n+1
-        print(n)
-    
-    if n/6 < 0.5:
-        return 'c' # Betray if severely punished last time,
-    else:
-        return 'b' # otherwise collude
+        n = 0
+        for thing in last6rounds:
+            letter = thing
+            if letter == 'c':
+                n=n+1
+        
+        if n > 3:
+            return 'c' # Betray if severely punished last time,
+        else:
+            return 'b' # otherwise collude
